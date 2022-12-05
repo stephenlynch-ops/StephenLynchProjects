a26631e46e898478d39a6807eea9b7db75b7669a
Stephen Lynch <stephen.lynch17@icloud.com>
2022-12-05 15:00:13 +0000

    Adjusted styling to keep images space equal on all cards

--- projects/forms.py
+++ projects/forms.py
@@ -8,6 +8,8 @@ class ProjectForm(forms.ModelForm):
         model = Project
         fields = '__all__'
 
-    image1 = forms.ImageField(label='Image1', required=False)
-    image2 = forms.ImageField(label='Image2', required=False)
-    image3 = forms.ImageField(label='Image3', required=False)
+    image1 = forms.ImageField(label='Image 1', required=False)
+    image2 = forms.ImageField(label='Image 2', required=False)
+    image3 = forms.ImageField(label='Image 3', required=False)
+    image4 = forms.ImageField(label='Image 4', required=False)
+    image5 = forms.ImageField(label='Image 5', required=False)
